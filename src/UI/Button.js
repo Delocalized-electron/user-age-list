7c6bb0e342a5fb1221413d54846e87bd5e8e1528
Aadil Khatri <aadil.khatri343@gmail.com>
2022-09-23 19:49:45 +0530

    Created reusable button component

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,7 +1,15 @@
 import React from "react";
-import classes from './Button.module.css'
-const Button = props =>{
-    return <button className={`${classes.button} ${props.className}`}>{props.value}</button>
+import classes from "./Button.module.css";
+const Button = (props) => {
+  return (
+    <button
+      className={`${classes.button} ${props.className}`}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
-export default Button
+export default Button;
